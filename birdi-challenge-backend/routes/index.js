03d744ae5b6300ccb75894acc1476e85760eda84
Hannah Greenwell <hannah.greenwell@gmail.com>
2019-01-10 23:47:46 +1100

    updated GET route to weather/:lat/:lng to retrieve current weather data from a third-party api and save the returned data to the local database

--- birdi-challenge-backend/routes/index.js
+++ birdi-challenge-backend/routes/index.js
@@ -11,32 +11,36 @@ dotenv.config();
 
 const API_KEY = process.env.OPEN_WEATHER_API;
 
+
 // get request for the current weather
 router.get('/weather/:lat/:lng', (request, response, next) => {
   const { lat, lng } = request.params;
   const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
   // makes a get request to the Open Weather API for the current weather
-  axios.get(`${BASE_URL}/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`)
-  .then(response => {
-    console.log(response.data)
+  axios.get(`${BASE_URL}/weather?lat=${lat}&lon=${lng}&units=metric&appid=${API_KEY}`)
+  .then(res => {
+    const temp = res.data.main.temp;
+    const wind = res.data.wind.speed;
+
+    // stores the weather data returned from the api request to the local database
+    db.query(
+      'INSERT INTO weather (location_id, temp, wind_speed) VALUES ($1, $2, $3) RETURNING *',
+      [1, temp, wind],
+      (error, result) => {
+        if (error) {
+          return next(error);
+        }
+
+        response.send(result.rows[0]);
+      }
+    );
   })
   .catch(error => {
     console.log(error);
   });
-
-  // db.query(
-  //   'SELECT * FROM location INNER JOIN weather ON weather.location_id = location.location_id WHERE lat = $1 AND lng = $2',
-  //   [lat, lng],
-  //   (error, result) => {
-  //     if (error) {
-  //       return next(error);
-  //     }
-  //     response.send(result.rows);
-  // });
-
-  response.send('HI');
 });
+
 
 router.get('/weather', (request, response, next) => {
   db.query('SELECT * FROM weather', (error, result) => {
@@ -44,17 +48,6 @@ router.get('/weather', (request, response, next) => {
       return next(error);
     }
     response.send(result.rows);
-  });
-});
-
-router.get('/weather/:id', (request, response, next) => {
-  const { id } = request.params;
-
-  db.query('SELECT * FROM weather WHERE report_id = $1', [id], (error, result) => {
-    if (error) {
-      return next(error);
-    }
-    response.send(result.rows[0]);
   });
 });
 
