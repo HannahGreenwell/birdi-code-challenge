e795c24bbf8090d179aea0706bcb9f7c86da40d8
Hannah Greenwell <hannah.greenwell@gmail.com>
2019-01-11 07:11:53 +1100

    updated GET route to weather/:lat/:lng so that the insert statement now inserts values for all columns in the table

--- birdi-challenge-backend/database.sql
+++ birdi-challenge-backend/database.sql
@@ -3,30 +3,48 @@ CREATE DATABASE birdi_challenge;
 
 \c birdi_challenge;
 
+-- CREATE TABLE location (
+--   location_id SERIAL PRIMARY KEY,
+--   lat DECIMAL NOT NULL,
+--   lng DECIMAL NOT NULL
+-- );
+
 CREATE TABLE location (
-  location_id SERIAL PRIMARY KEY,
   lat DECIMAL NOT NULL,
-  lng DECIMAL NOT NULL
+  lng DECIMAL NOT NULL,
+  PRIMARY KEY (lat, lng)
 );
 
 INSERT INTO location (lat, lng)
   VALUES
     (-33.886249, 151.200251),
     (-33.771841, 150.906187),
-    (-33.798178, 151.184079);
+    (-33.798178, 151.184079),
+    (22.327331, 114.171973);
+
+-- CREATE TABLE weather (
+--   report_id SERIAL PRIMARY KEY,
+--   location_id INTEGER REFERENCES location (location_id),
+--   temp DECIMAL,
+--   wind_speed DECIMAL,
+--   chance_of_rain INTEGER,
+--   description VARCHAR(100),
+--   created_at TIMESTAMP
+-- );
 
 CREATE TABLE weather (
   report_id SERIAL PRIMARY KEY,
-  location_id INTEGER REFERENCES location (location_id),
+  lat DECIMAL,
+  lng DECIMAL,
+  description VARCHAR(100),
   temp DECIMAL,
   wind_speed DECIMAL,
-  chance_of_rain INTEGER,
-  description VARCHAR(100),
-  created_at TIMESTAMP
+  created_at TIMESTAMP,
+  FOREIGN KEY (lat, lng) REFERENCES location (lat, lng)
 );
 
-INSERT INTO weather (location_id, temp, wind_speed, chance_of_rain, description, created_at)
-  VALUES
-    (1, 22.5, 15.2, 75, 'showers', current_timestamp),
-    (2, 25.5, 13.8, 68, 'cloudy', current_timestamp),
-    (3, 21.2, 16.6, 88, 'showers', current_timestamp);
+-- INSERT INTO weather (location_id, temp, wind_speed, chance_of_rain, description, created_at)
+--   VALUES
+--     (1, 22.5, 15.2, 75, 'showers', current_timestamp),
+--     (2, 25.5, 13.8, 68, 'cloudy', current_timestamp),
+--     (3, 21.2, 16.6, 88, 'showers', current_timestamp);
--- birdi-challenge-backend/routes/index.js
+++ birdi-challenge-backend/routes/index.js
@@ -17,16 +17,18 @@ router.get('/weather/:lat/:lng', (request, response, next) => {
   const { lat, lng } = request.params;
   const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
-  // makes a get request to the Open Weather API for the current weather
+  // makes a get request to the Open Weather api for the current weather
   axios.get(`${BASE_URL}/weather?lat=${lat}&lon=${lng}&units=metric&appid=${API_KEY}`)
   .then(res => {
+    const description = res.data.weather.description;
     const temp = res.data.main.temp;
-    const wind = res.data.wind.speed;
+    const wind_speed = res.data.wind.speed;
+    const created_at = new Date();
 
     // stores the weather data returned from the api request to the local database
     db.query(
-      'INSERT INTO weather (location_id, temp, wind_speed) VALUES ($1, $2, $3) RETURNING *',
-      [1, temp, wind],
+      'INSERT INTO weather (lat, lng, description, temp, wind_speed, created_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+      [lat, lng, description, temp, wind_speed, created_at],
       (error, result) => {
         if (error) {
           return next(error);
@@ -38,16 +40,6 @@ router.get('/weather/:lat/:lng', (request, response, next) => {
   })
   .catch(error => {
     console.log(error);
-  });
-});
-
-
-router.get('/weather', (request, response, next) => {
-  db.query('SELECT * FROM weather', (error, result) => {
-    if (error) {
-      return next(error);
-    }
-    response.send(result.rows);
   });
 });
 
