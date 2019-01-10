1ce988b49a087d0d3176791c80e7007a4c668f85
Hannah Greenwell <hannah.greenwell@gmail.com>
2019-01-10 23:11:31 +1100

    installed axios to make api request for current weather data and added gitignore file

--- birdi-challenge-backend/database.sql
+++ birdi-challenge-backend/database.sql
@@ -4,25 +4,29 @@ CREATE DATABASE birdi_challenge;
 \c birdi_challenge;
 
 CREATE TABLE location (
-  lat DECIMAL,
-  lng DECIMAL,
-  PRIMARY KEY (lat, lng)
+  location_id SERIAL PRIMARY KEY,
+  lat DECIMAL NOT NULL,
+  lng DECIMAL NOT NULL
 );
 
 INSERT INTO location (lat, lng)
-  VALUES (-33.886249, 151.200251);
+  VALUES
+    (-33.886249, 151.200251),
+    (-33.771841, 150.906187),
+    (-33.798178, 151.184079);
 
 CREATE TABLE weather (
   report_id SERIAL PRIMARY KEY,
-  lat DECIMAL,
-  lng DECIMAL,
+  location_id INTEGER REFERENCES location (location_id),
   temp DECIMAL,
   wind_speed DECIMAL,
   chance_of_rain INTEGER,
   description VARCHAR(100),
-  created_at TIMESTAMP,
-  FOREIGN KEY (lat, lng) REFERENCES location (lat, lng)
+  created_at TIMESTAMP
 );
 
-INSERT INTO weather (lat, lng, temp, wind_speed, chance_of_rain, description, created_at)
-  VALUES (-33.886249, 151.200251, 22.5, 15.2, 75, 'showers', current_timestamp);
+INSERT INTO weather (location_id, temp, wind_speed, chance_of_rain, description, created_at)
+  VALUES
+    (1, 22.5, 15.2, 75, 'showers', current_timestamp),
+    (2, 25.5, 13.8, 68, 'cloudy', current_timestamp),
+    (3, 21.2, 16.6, 88, 'showers', current_timestamp);
--- birdi-challenge-backend/routes/index.js
+++ birdi-challenge-backend/routes/index.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 // require database adapter file
 const db = require('../db');
 
-router.get('/', (request, response) => response.json({ status: "ok" }));
+// gives access to variables in .env
+const dotenv = require('dotenv');
+dotenv.config();
 
-router.get('/weather/:lat/:lng', (request, response) => {
-  console.log(request.params);
-  response.send('HI!');
+const API_KEY = process.env.OPEN_WEATHER_API;
+
+// get request for the current weather
+router.get('/weather/:lat/:lng', (request, response, next) => {
+  const { lat, lng } = request.params;
+  const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+
+  // makes a get request to the Open Weather API for the current weather
+  axios.get(`${BASE_URL}/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`)
+  .then(response => {
+    console.log(response.data)
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+  // db.query(
+  //   'SELECT * FROM location INNER JOIN weather ON weather.location_id = location.location_id WHERE lat = $1 AND lng = $2',
+  //   [lat, lng],
+  //   (error, result) => {
+  //     if (error) {
+  //       return next(error);
+  //     }
+  //     response.send(result.rows);
+  // });
+
+  response.send('HI');
 });
 
 router.get('/weather', (request, response, next) => {
